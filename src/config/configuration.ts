7dca2274f1f5939bf0ef9bb8dee59fdd33823489
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-02 05:24:18 +0400

    feat: get port via configuration class

---
+++ src/config/configuration.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class Configuration {
+  get port(): number {
+    return parseInt(process.env.PORT || '4000', 10);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Configuration } from './config/configuration';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const config = app.get(Configuration);
+  await app.listen(config.port);
 }
 bootstrap();
