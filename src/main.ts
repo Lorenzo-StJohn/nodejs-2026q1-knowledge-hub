a3ff0af7c918f58c9e3fb2fe57db4c574ce54892
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-03 15:33:32 +0400

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { join } from 'node:path';
+
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
+import * as yaml from 'js-yaml';
 
 import { AppModule } from './app.module';
 import { Configuration } from './config/configuration';
+import { readFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +18,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const yamlPath = join(__dirname, '..', 'doc', 'api.yaml');
+  const yamlContent = await readFile(yamlPath, 'utf8');
+  const document = yaml.load(yamlContent) as OpenAPIObject;
+
+  SwaggerModule.setup('doc', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   const config = app.get(Configuration);
   await app.listen(config.port);
