cca1bec8ad407c5045f303e4b4da263ea20d9773
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-04 12:34:54 +0400

    refactor: update swagger for comment endpoint

--- src/modules/comment/dto/find-comment-query.dto.ts
+++ src/modules/comment/dto/find-comment-query.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsIn, IsOptional, IsUUID } from 'class-validator';
 
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
@@ -12,15 +13,29 @@ export const CommentFields = [
 ];
 
 export class FindCommentQueryDto extends PaginationQueryDto {
+  @ApiProperty({
+    description: 'Article ID to get comments for',
+    example: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+  })
   @IsUUID('4', {
     message: 'ArticleId should be valid UUID v4',
   })
   articleId: string;
 
+  @ApiPropertyOptional({
+    description: 'The field to sort by',
+    enum: CommentFields,
+    example: 'content',
+  })
   @IsOptional()
   @IsIn(CommentFields)
   sortBy?: (typeof CommentFields)[number];
 
+  @ApiPropertyOptional({
+    description: 'Sorting direction',
+    enum: Order,
+    example: 'DESC',
+  })
   @IsOptional()
   @IsIn(Order)
   order?: (typeof Order)[number] = Order[0];
