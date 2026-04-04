48849dead2c994b82511c5cb84deebb8bf65b23d
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-04 23:33:36 +0400

    feat: add doc link to default route response

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Go to http://localhost:4000/doc/ see documentation (replace 4000 with your port)';
   }
 }
